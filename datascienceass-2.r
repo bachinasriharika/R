1db8827e74b14a382f06b17dacb45018ae086306
bachinasriharika <110109962+bachinasriharika@users.noreply.github.com>
2022-09-19 19:49:30 +0530

    Add files via upload

---
+++ datascienceass-2.r
@@ -0,0 +1,48 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL                            ## initialize the inverse property
+  
+  set <- function( matrix ) {          ## to set the matrix
+    m <<- matrix
+    i <<- NULL
+  }
+  
+  get <- function() m                  ## to get the matrix
+    
+  setInverse <- function(inverse) {     ## to set the inverse matrix
+    i <<- inverse
+  }
+  
+  getInverse <- function() i            ## to get the inverse matrix
+
+  list(set = set, get = get,            ## return a list of the methods
+       setInverse = setInverse,
+       getInverse = getInverse)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+  
+  
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()               ## get the matrix from data
+  
+  m <- solve(data) %*% data     ## calculate the inverse
+  
+  x$setInverse(m)               ## setting the inverse to the object
+  
+  m                             ## Return the matrix
+}
